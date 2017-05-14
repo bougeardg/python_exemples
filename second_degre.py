10610c2e20d8ce2b06065b6c2fe3c6d6d6752b87
bougeardg <bougeardg@yahoo.fr>
2017-05-14 12:29:16 +0200

    protection si a=0 calcul de l’extremum

--- second_degre.py
+++ second_degre.py
@@ -5,17 +5,33 @@ def disc(a,b,c):
 a=int(input("coefficient de x^2 ? "))
 b=int(input("coefficient de x ? "))
 c=int(input("coefficient constant ?"))
-d=disc(a,b,c)
-print("discriminant de ", a, "x^2+",b,"x+", c, "= ",d)
 
-if d>0:
-    print("le polynôme a deux racines :")
-    e=(-b-math.sqrt(d))/(2*a)
-    f=(-b+ math.sqrt(d))/(2*a)
-    print(e, " et ",f)
-elif d==0:
-    print("le polynôme a une racine :")
-    e=-b/(2*a)
-    print(e)
-else :
-    print(" le polynôme n'a aucune racine réelle")
+
+
+if a!=0:
+    #recherche des racines réelles
+    d=disc(a,b,c)
+    print("discriminant de ", a, "x^2+",b,"x+", c, "= ",d)
+    if d>0:
+        print("le polynôme a deux racines :")
+        e=(-b-math.sqrt(d))/(2*a)
+        f=(-b+ math.sqrt(d))/(2*a)
+        print(e, " et ",f)
+    elif d==0:
+        print("le polynôme a une racine :")
+        e=-b/(2*a)
+        print(e)
+    else :
+        print("le polynôme n'a aucune racine réelle")
+    # Variations
+    g=-b/(2*a)
+    h=a*g**2+b*g+c
+    if a>0:
+        print("la fonction polynôme admet un minimum en",g)
+        print("il vaut ", h)
+    else:
+        print("la fonction polynôme admet un maximums en",g)
+        print("il vaut ", h)        
+else:
+    print("ceci n'est pas un polynôme du second degré")
+
