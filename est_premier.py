0d5301a5eedf9c7961bb0e6471c433df8f68aee5
bougeardg <bougeardg@yahoo.fr>
2017-05-17 20:15:54 +0200

    documentation

--- est_premier.py
+++ est_premier.py
@@ -10,6 +10,8 @@ def aff(l):
 
 
 def est_premier(n,l):
+    """ teste si un entier est premier en le testant
+    avec les diviseurs de la liste l qui sont sensé être premiers"""
     test=True
     i=0
     while l[i]<math.sqrt(n)and i<len(l):
@@ -27,17 +29,6 @@ def l_prem(k):
             l.append(i)
     return l
 
-"""with open('liste_premiers','wb') as fichier:
-    liste_premiers=pickle.Pickler(fichier)
-    liste_premiers.dump(l_prem(10))
-
-with open('liste_premiers','rb') as fic:
-    lp=pickle.Unpickler(fic)
-    liste_recupere=lp.load()
-
-print('recup',liste_recupere)
-
-"""
 
 reponse=input("Est-ce la première utilisation du programme ? (oui/non)")
 if reponse.lower()=="oui":
@@ -45,14 +36,16 @@ if reponse.lower()=="oui":
         liste_premiers=pickle.Pickler(fichier)
         liste_premiers.dump(l_prem(10))
 
-with open('liste_premiers','rb') as fic:
+with open('liste_premiers','rb') as fic:#chargement depuis le fichier
     lp=pickle.Unpickler(fic)
     l_r=lp.load()
+
 print("liste de nombre premier actuelle : ")
 print(l_r)
+
 test=True
-while test:
-    k=int(input("combien de termes en plus ? (0 pour arrèter)"))
+while test:# c'est parti pour l'agrandissement de la liste !
+    k=int(input("combien de termes en plus ? (0 pour arrêter)"))
     if k==0:
         test=False
     nb_de_nb=0
@@ -63,6 +56,7 @@ while test:
             i=i+1
         l_r.append(i)
         cpt=cpt+1
+#affichage-enregistrement de la liste finale
 print(l_r)
 with open('liste_premiers','wb') as fichier:
     liste_premiers=pickle.Pickler(fichier)
