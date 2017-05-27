c4027d564875938d79a01d8e501fa3583d497928
bougeardg <bougeardg@yahoo.fr>
2017-05-27 10:26:11 +0200

    mandelbrot : nouvelle version qui permet de sauvegarder les images crées première utilisation des calculs de fractions avec python

---
+++ calculs_fractions.py
@@ -0,0 +1,16 @@
+from fractions import *
+#définition de deux nombres comme des fractions
+a=Fraction(1,3)
+b=Fraction(1,4)
+#affichage et opérations
+print(a,"+",b,"=",a+b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+print(a,"%",b,"=",a%b)#reste euclidien
+print(a,"^12=",a**12)
+print(Fraction(4,6))#la réduction est automatique
+
+
+
+
+
